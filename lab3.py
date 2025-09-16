781252cc0e83d1431635e39322bfb3827870b20a
farkanhussain <farkanhussain@gmail.com>
2025-09-16 14:33:23 +0100

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,12 +1,12 @@
 from collections import Counter
 
 def analyse_sentence(sentence):
-words= sentence.lower().split()
-total_words=len(words)
-unique_words=len(set(words))
-most_common= Counter(words).most_common(1)[0]
+    words= sentence.lower().split()
+    total_words=len(words)
+    unique_words=len(set(words))
+    most_common= Counter(words).most_common(1)[0]
 
-return total_words, unique_words, most_common
+    return total_words, unique_words, most_common
 
 sentence="The quick brown fox jumped over the lazy dog"
 total,unique,common=analyse_sentence(sentence)
