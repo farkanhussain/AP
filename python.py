5cd74337ce25cb51de14dadd2c42ca8caf440397
farkanhussain <farkanhussain@gmail.com>
2025-09-16 13:38:49 +0100

    Create python.py

---
+++ python.py
@@ -0,0 +1,13 @@
+try:
+    a=float(input("Enter your first number: "))
+    b=float(input("Enter your second number: "))
+    print(f"Sum: {a + b}")
+    print(f"Difference: {a - b})
+    print(f"Product: {a * b})
+
+    if b != 0:
+        print(f"Division: {a/b:.2f}")
+    else:
+         print("Error: Division by zero is not allowed.")
+except ValueError:
+print("Invalid input.Please enter numeric values.")
