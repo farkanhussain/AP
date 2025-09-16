cde596e97a4d0cb65f3468a96cd433029223096b
farkanhussain <farkanhussain@gmail.com>
2025-09-16 13:42:28 +0100

    Update python.py

--- python.py
+++ python.py
@@ -2,8 +2,8 @@ try:
     a=float(input("Enter your first number: "))
     b=float(input("Enter your second number: "))
     print(f"Sum: {a + b}")
-    print(f"Difference: {a - b})
-    print(f"Product: {a * b})
+    print(f"Difference: {a - b}")
+    print(f"Product: {a * b}")
 
     if b != 0:
         print(f"Division: {a/b:.2f}")
