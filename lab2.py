8b15dae825d00df582555e7c3ae96fec2c127c5e
farkanhussain <farkanhussain@gmail.com>
2025-09-16 14:08:40 +0100

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,9 @@
+Numbers= [12,5,8,22,4,10,25,9,8,3]
+
+print("Largest: ",max(Numbers))
+print("Smallest: ",min(Numbers))
+print("Average: ",sum(Numbers) / len(Numbers))
+
+num_set= set(Numbers)
+print("Original list: ", Numbers)
+print(" Sorted list: ", num_set)
